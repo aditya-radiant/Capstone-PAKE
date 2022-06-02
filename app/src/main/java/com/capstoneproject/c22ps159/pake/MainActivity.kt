21f24aa21088384812b9b37e1a18c1ea0b398cee
aditya-radiant <77778937+aditya-radiant@users.noreply.github.com>
2022-06-02 10:32:53 +0700

    Initial commit

---
+++ app/src/main/java/com/capstoneproject/c22ps159/pake/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.capstoneproject.c22ps159.pake
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
