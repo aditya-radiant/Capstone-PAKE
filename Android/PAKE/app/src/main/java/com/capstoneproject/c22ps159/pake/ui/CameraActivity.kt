7b47c69906ea04de578dd92239a46be3a2d1ba1b
aditya-radiant <77778937+aditya-radiant@users.noreply.github.com>
2022-06-03 22:37:32 +0700

    test

---
+++ Android/PAKE/app/src/main/java/com/capstoneproject/c22ps159/pake/ui/CameraActivity.kt
@@ -0,0 +1,12 @@
+package com.capstoneproject.c22ps159.pake.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.capstoneproject.c22ps159.pake.R
+
+class CameraActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_camera)
+    }
+}
