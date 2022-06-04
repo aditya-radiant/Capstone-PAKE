87250130210d5910511f036513f0771d75355c50
Zwingli09 <zwingli.hilkia@gmail.com>
2022-06-04 14:39:45 +0700

    Add activity_forget_password.xml, ForgetPasswordActivity.kt, edit name edit text also add some string.

---
+++ Android/PAKE/app/src/main/java/com/capstoneproject/c22ps159/pake/ui/forgetpassword/ForgetPasswordActivity.kt
@@ -0,0 +1,12 @@
+package com.capstoneproject.c22ps159.pake.ui.forgetpassword
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.capstoneproject.c22ps159.pake.R
+
+class ForgetPasswordActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_forget_password)
+    }
+}
