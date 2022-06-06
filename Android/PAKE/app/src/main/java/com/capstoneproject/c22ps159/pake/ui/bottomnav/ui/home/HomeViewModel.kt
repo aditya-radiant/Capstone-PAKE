8e13d6ca1715acaf2ff8c36bc956545adfca4f99
aditya-radiant <77778937+aditya-radiant@users.noreply.github.com>
2022-06-06 18:07:28 +0700

    Add bottom nav etc

---
+++ Android/PAKE/app/src/main/java/com/capstoneproject/c22ps159/pake/ui/bottomnav/ui/home/HomeViewModel.kt
@@ -0,0 +1,13 @@
+package com.capstoneproject.c22ps159.pake.ui.bottomnav.ui.home
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is home Fragment"
+    }
+    val text: LiveData<String> = _text
+}
