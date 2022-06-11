fadd2d9c3d032424cb67bf3a81c9ebd3e2025ceb
aditya-radiant <77778937+aditya-radiant@users.noreply.github.com>
2022-06-11 21:59:26 +0700

    Adding camera + edit customview

--- Android/PAKE/app/src/main/java/com/capstoneproject/c22ps159/pake/ui/customview/NameEditText.kt
+++ Android/PAKE/app/src/main/java/com/capstoneproject/c22ps159/pake/ui/customview/NameEditText.kt
@@ -2,20 +2,17 @@ package com.capstoneproject.c22ps159.pake.ui.customview
 
 import android.content.Context
 import android.graphics.Canvas
-import android.graphics.drawable.Drawable
 import android.text.Editable
 import android.text.TextWatcher
 import android.util.AttributeSet
-import android.view.MotionEvent
 import android.view.View
 import androidx.core.content.ContextCompat
 import com.capstoneproject.c22ps159.pake.R
 import com.google.android.material.textfield.TextInputEditText
 
-class NameEditText: TextInputEditText, View.OnTouchListener {
+class NameEditText: TextInputEditText{
     private val roundedBackground =
         ContextCompat.getDrawable(context, R.drawable.bg_edttext)
-    private lateinit var clearButtonImage: Drawable
 
     constructor(context: Context) : super(context) {
         init()
@@ -34,83 +31,26 @@ class NameEditText: TextInputEditText, View.OnTouchListener {
     }
 
     private fun init() {
-        clearButtonImage = ContextCompat.getDrawable(context, R.drawable.ic_close) as Drawable
-
-        setOnTouchListener(this)
 
         addTextChangedListener(object : TextWatcher {
-            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
 
             override fun onTextChanged(text: CharSequence?, p1: Int, p2: Int, p3: Int) {
-
             }
 
             override fun afterTextChanged(text: Editable?) {
                 text?.let {
                     if (text.isEmpty())
-                        error = context.applicationContext.getString(R.string.fill_the_name)
-                        hideClearButton()
-                    if (text.toString().isNotEmpty()) showClearButton()
+                        error = context.applicationContext.getString(R.string.fill_the_form)
                 }
             }
         })
     }
 
-    // Menampilkan clear button
-    private fun showClearButton() {
-        setButtonDrawables(startOfTheText = clearButtonImage)
-    }
-
-    // Menghilangkan clear button
-    private fun hideClearButton() {
-        setButtonDrawables()
-    }
-
-    //Mengkonfigurasi button
-    private fun setButtonDrawables(startOfTheText: Drawable? = null, topOfTheText:Drawable? = null, endOfTheText:Drawable? = null, bottomOfTheText: Drawable? = null){
-        setCompoundDrawablesWithIntrinsicBounds(startOfTheText, topOfTheText, endOfTheText, bottomOfTheText)
-    }
-
-    override fun onTouch(v: View?, event: MotionEvent): Boolean {
-        if (compoundDrawables[2] != null) {
-            val clearButtonStart: Float
-            val clearButtonEnd: Float
-            var isClearButtonClicked = false
-
-            if (layoutDirection == View.LAYOUT_DIRECTION_RTL) {
-                clearButtonEnd = (width - paddingEnd - clearButtonImage.intrinsicWidth).toFloat()
-                when {
-                    event.x > clearButtonEnd -> isClearButtonClicked = true
-                }
-            } else {
-                clearButtonStart = (clearButtonImage.intrinsicWidth + paddingStart).toFloat()
-                when {
-                    event.x < clearButtonStart -> isClearButtonClicked = true
-                }
-            }
-            if (isClearButtonClicked) {
-                when (event.action) {
-                    MotionEvent.ACTION_DOWN -> {
-                        clearButtonImage = ContextCompat.getDrawable(context, R.drawable.ic_close) as Drawable
-                        showClearButton()
-                        return true
-                    }
-                    MotionEvent.ACTION_UP -> {
-                        clearButtonImage = ContextCompat.getDrawable(context, R.drawable.ic_close) as Drawable
-                        text?.clear()
-                        hideClearButton()
-                        return true
-                    }
-                    else -> return false
-                }
-            } else return false
-        }
-        return false
-    }
-
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
-        setBackgroundResource(R.drawable.bg_edttext_border_color)
+        background = roundedBackground
         textSize = 14F
         textAlignment = View.TEXT_ALIGNMENT_VIEW_START
     }
